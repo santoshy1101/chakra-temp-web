0ae51871c7dba3c7caa2f2e0b3a7ac12435989d0
santosh <styadav018@gmail.com>
2023-05-20 20:20:29 +0530

    removed after page refrshed error

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -25,7 +25,7 @@ export default function Card() {
   return (
     <Grid
       templateColumns={{
-        base: 'repeat(2, 1fr)',
+        base: 'repeat(1, 1fr)',
         sm: 'repeat(2, 1fr)',
         md: 'repeat(3, 1fr)',
         lg: 'repeat(4, 1fr)',
